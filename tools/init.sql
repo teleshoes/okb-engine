8b2882eb63783b9260a31e4a47c6ba85c07d0245
Eric Berenguier <eric.berenguier@free.fr>
2014-07-17 21:00:30 +0200

    new sql schema (btw why are we using sql ?)

--- tools/init.sql
+++ tools/init.sql
@@ -2,6 +2,7 @@ DROP INDEX IF EXISTS words_idx;
 DROP INDEX IF EXISTS grams_idx;
 DROP TABLE IF EXISTS grams;
 DROP TABLE IF EXISTS words;
+DROP TABLE IF EXISTS params;
 DROP TABLE IF EXISTS version;
 
 CREATE TABLE grams (
@@ -9,18 +10,24 @@ CREATE TABLE grams (
    id2 INT NOT NULL,
    id3 INT NOT NULL,
    stock_count INT NOT NULL,
-   user_count INT,
-   user_last INT
+   user_count REAL,
+   user_replace REAL,
+   last_time INT
 );
 
 CREATE INDEX grams_idx ON grams (id1, id2, id3);
 
 CREATE TABLE words (
    id INT PRIMARY KEY NOT NULL,
-   word VARCHAR(50) NOT NULL
+   word TEXT NOT NULL
 );
 
 CREATE INDEX words_idx ON words (word);
+
+CREATE TABLE params (
+    key TEXT NOT NULL,
+    value REAL
+);
 
 CREATE TABLE version (
    version INT
