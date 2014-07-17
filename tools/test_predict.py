172265bb72fbe30db2e950c7333d90e46d7cb56b
Eric Berenguier <eric.berenguier@free.fr>
2014-07-17 21:03:49 +0200

    test_predict can be used with new predict.py version

--- tools/test_predict.py
+++ tools/test_predict.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import sys, os, re
+import getopt
 
 mydir = os.path.dirname(os.path.abspath(__file__))
 libdir = os.path.join(mydir, '..')
@@ -9,22 +10,32 @@ sys.path.insert(0, libdir)
 
 import predict
 
+lang = "en"
+opts, args =  getopt.getopt(sys.argv[1:], 'l:')
+for o, a in opts:
+    if o == "-l":
+        lang = a
+    else:
+        print("Bad option: %s", o)
+        exit(1)
 
-line = ' '.join(sys.argv[1:])
-words = re.split(r'[^a-zA-Z\-\']+', line)
+line = ' '.join(args)
+words = re.split(r'[^\w\-\']+', line)
 words = [ x for x in words if len(x) > 0 ]
 
 print("Words:", words)
 
-p = predict.p
+p = predict.Predict()
 
 last_word = words.pop(-1)
 
-p.set_dbfile(os.path.join(mydir, "../db/predict-en.db"))
+p.set_dbfile(os.path.join(mydir, "../db/predict-%s.db" % lang))
+p.load_db()
 tmp = ' '.join(words)
 p.update_surrounding(tmp, len(tmp))
 p.update_preedit("")
 p._update_last_words()
+print("Context:", p.last_words)
 
 score = p._get_all_predict_scores([last_word])
 
