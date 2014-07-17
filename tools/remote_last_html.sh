6846d372059c8574c00a3b97de3c1cae12f0e177
Eric Berenguier <eric.berenguier@free.fr>
2014-07-17 21:02:37 +0200

    Changement de nom des fichiers de log

--- tools/remote_last_html.sh
+++ tools/remote_last_html.sh
@@ -3,5 +3,5 @@
 HOST=192.168.2.16 # <- my jolla usb ip address
 d=`dirname "$0"`
 tmp=`mktemp /tmp/curvekb.XXXXXX.html`
-ssh nemo@$HOST 'tail -n 10 /tmp/curvekb.log | grep "^OUT:" | tail -n 1' | sed 's/^OUT:\ *//' | $d/jsonresult2html.py > $tmp && firefox "$tmp"
+ssh nemo@$HOST 'tail -n 10 souk/okboard/curve.log | grep "^OUT:" | tail -n 1' | sed 's/^OUT:\ *//' | $d/jsonresult2html.py > $tmp && firefox "$tmp"
 
