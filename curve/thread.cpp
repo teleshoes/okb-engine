0a0049f976a5d8277940e83ccb685e797bd4cc2d
Eric Berenguier <eric.berenguier@free.fr>
2014-08-01 19:28:27 +0200

    statistics about incremental processing

--- curve/thread.cpp
+++ curve/thread.cpp
@@ -1,4 +1,17 @@
 #include "thread.h"
+#include <sys/time.h>
+#include <sys/resource.h>
+
+
+static float cpu_user(struct rusage &start, struct rusage &stop) {
+  return (float) (stop.ru_utime.tv_sec - start.ru_utime.tv_sec) +    
+    (float) (stop.ru_utime.tv_usec - start.ru_utime.tv_usec) / 1000000.0;
+}
+
+static float cpu_sys(struct rusage &start, struct rusage &stop) {
+  return (float) (stop.ru_stime.tv_sec - start.ru_stime.tv_sec) +    
+    (float) (stop.ru_stime.tv_usec - start.ru_stime.tv_usec) / 1000000.0;
+}
 
 CurveThread::CurveThread(QObject *parent) :
   QThread(parent)
@@ -36,6 +49,8 @@ void CurveThread::stopThread() {
 }
 
 void CurveThread::waitForIdle() {
+  /* this is mostly for tests, because in real life a user may start a new word swipe even before the previous one is matched.
+     real production application must use callback feature */
   mutex.lock();
   if (! idle) {
     idleCondition.wait(&mutex);
@@ -52,6 +67,13 @@ void CurveThread::run() {
   qDebug("thread starting ...");
   QList<CurvePoint> inProgress;
   bool started = false;
+
+  /* statistics & counters */
+  struct rusage ru_started;
+  struct rusage ru_completed;
+  struct rusage ru_matched;
+  QTime t_completed;
+  QTime t_matched;
 
   forever {
     inProgress.clear();
@@ -79,13 +101,32 @@ void CurveThread::run() {
 	started = false;
       } else if (point.x == CMD_END) {
 	int id = point.y;
+
+	t_completed = QTime::currentTime();
+	getrusage(RUSAGE_THREAD, &ru_completed);
+
 	matcher->endCurve(id);
+
+	t_matched = QTime::currentTime();
+	getrusage(RUSAGE_THREAD, &ru_matched);
+	
+	// display statistics
+	QList<CurvePoint> curve = matcher->getCurve();
+	qDebug("+++ CPU time before=%.3f/%.3f after=%.3f/%.3f draw-time=%.3f end-time=%.3f",
+	       cpu_user(ru_started, ru_completed), cpu_sys(ru_started, ru_completed),
+	       cpu_user(ru_completed, ru_matched), cpu_sys(ru_completed, ru_matched),
+	       ((float)(curve[curve.size() - 1].t - curve[0].t)) / 1000,
+	       (float)(t_completed.msecsTo(t_matched)) / 1000);
+	
 	started = false;
 	// @todo push back result !
       } else if (point.x == CMD_QUIT) {
 	qDebug("thread exiting ...");
 	return;	
       } else {
+	if (! started) {
+	  getrusage(RUSAGE_THREAD, &ru_started);
+	}
 	started = true;
 	matcher->addPoint(point);
       }
