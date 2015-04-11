9807d6732849d39c7a7ab79871ba9b0d42cd5668
Eric Berenguier <eric.berenguier@free.fr>
2015-04-11 08:11:26 +0200

    Langage files clean option to force a full rebuild

--- db/build.sh
+++ db/build.sh
@@ -19,13 +19,18 @@ die() { echo "DIE: $*"; exit 1; }
 [ -d "$WORK_DIR" ] || die "working directory not found: $WORK_DIR"
 [ -d "$CORPUS_DIR" ] || die "corpus directory not found: $CORPUS_DIR"
 
+add_tgt=
+if [ -n "$1" ] ; then
+    if [ "$1" = "-r" ] ; then add_tgt=clean ; else die "bad command line option: $1, only supported option is '-r' to force a full rebuild" ; fi
+fi
+
 cd `dirname "$0"`
 mydir=`pwd`
 
 cp -vauf $mydir/lang-*.cf $mydir/db.version $WORK_DIR/
 
 cd $WORK_DIR
-for target in depend all ; do
+for target in $add_tgt depend all ; do
     make -j -f $mydir/makefile CORPUS_DIR=${CORPUS_DIR} TOOLS_DIR=$mydir/../tools $target
 done
 
