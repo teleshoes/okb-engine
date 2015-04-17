4c82467f2bba56f2c70574e3b0c8ed52288cbe57
Eric Berenguier <eric.berenguier@free.fr>
2015-04-17 16:45:29 +0200

    Source code cleanup
    - Removed obsolete scripts (sqlite storage ...)
    - Moved scripts used for one-time experiments to tmp/ dir to reduce
      clutter in tools/ directory

--- tools/db_dump_user.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh -e
-
-db="$1"
-[ -z "$db" -o ! -f "$db" ] && echo "usage: "`basename "$0"`" <predict db file>" && exit 1
-
-set -x
-sqlite3 "$db" 'select w1.word, w2.word, w3.word, g.stock_count, g.user_count, g.user_replace, g.last_time from words w1, words w2, words w3, grams g where g.user_count > 0 and g.id1 = w1.id and g.id2 = w2.id and g.id3 = w3.id order by g.user_count'
-
--- tools/db_reset_sqlite.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh -e
-# remove all learning information (i.e. return to stock database)
-
-db="$1"
-[ -z "$db" -o ! -f "$db" ] && echo "usage: "`basename "$0"`" <predict db file>" && exit 1
-
-set -x
-sqlite3 "$db" 'update grams set user_count = 0, user_replace = 0, last_time = 0'
