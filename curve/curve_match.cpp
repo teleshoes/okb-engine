c6f97bc9603b8328b2695b4ee54c5012b63ee158
Eric Berenguier <eric.berenguier@free.fr>
2015-06-21 10:44:06 +0200

    Scenario filtering optimisation (incremental mode only)

--- curve/curve_match.cpp
+++ curve/curve_match.cpp
@@ -592,7 +592,9 @@ bool CurveMatch::match() {
     n += 1;
     scenarios = new_scenarios; // remember QT collections have intelligent copy-on-write (hope it works)
     if (n >= 3) {
-      scenarioFilter(scenarios, 0, 15, params.max_active_scenarios, false); // @todo add to parameter list
+      // scenario filtering works differently in non-incremental mode
+      // as we do not care about performance in this case, just arbitrary increase max_active_scenarios value
+      scenarioFilter(scenarios, 0, 15, params.max_active_scenarios * 1.5, false); // @todo add to parameter list
       DBG("Depth: %d - Scenarios: %d - Candidates: %d", n, scenarios.size(), candidates.size());
     }
   }
