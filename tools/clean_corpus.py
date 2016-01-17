2d9e6f4696279180fb49751b8f985ce24950c83f
Eric Berenguier <eric.berenguier@free.fr>
2016-01-17 12:28:30 +0100

    Corpus cleaner improvements (ljo patch applied)
    More Unicode punctiation signs mapped to ASCII

--- tools/clean_corpus.py
+++ tools/clean_corpus.py
@@ -14,12 +14,17 @@ while buf:
     line = re.sub('\r', '', line)
 
     # convert some UTF-8 character to ASCII equivalent (very incomplete)
-    line = re.sub(r'\u2013', '-', line)  # hyphen
-    line = re.sub(r'[\u2019\u2018]', "'", line)  # quote
-    line = re.sub(r'[\u201C\u201D]', '"', line)  # double-quote
-    line = re.sub(r'[\u2026\u22C5]', '.', line)  # ellipsis & dots
+    line = re.sub(r'[\u2010\u2011\u2012\u2013\u2014\u2015\u207B\u2212\uff5E]', '-', line)  # hyphen
+    line = re.sub(r'[\u030D\u2018\u2019\u201A\u2032\u2039\u203A]', "'", line)  # quote
+    line = re.sub(r'[\u030F\u02EE\u201C\u201D\u201E\u2033\u226A\u226B]', '"', line)  # double-quote
+    line = re.sub(r'[\u2022\u2026\u2092\u22C5]', '.', line)  # ellipsis & dots
 
-    line = re.sub(r'[^\ \!-\~\t\r\n\'\u0080-\u023F\u20AC]', ' ', line)
+    # maybe to be added when non latin charsets are supported:
+    # \u0572-\u0585, \u10dd-\u10eb, \u1d49-\u1ecd -> alpha
+    # \u0400-\u04FF (cyrillic)
+    # Smilies \u1f440-\u1f4aa, \u1f600-\1f633
+
+    line = re.sub(r'[^\ \!-\~\t\r\n\'\u0080-\u023F]', ' ', line)
 
     print(line, end = '')
 
