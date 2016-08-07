1a52674bfbfc0ff44aa660ae3e5be19bb8bcd035
Eric Berenguier <eric.berenguier@free.fr>
2016-08-07 16:53:37 +0200

    key shift feature optional in word test script

--- tools/word_test.sh
+++ tools/word_test.sh
@@ -2,24 +2,27 @@
 # just check curve matching plugin with expected word
 # (generates far less output)
 
+no_key_shift=
+[ "$1" == '-k' ] && shift && no_key_shift=1
+
 test="$1"
 word="$2"
-if [ ! -f "$test" ] ; then echo "usage: $0 <json test file> [<word to test>]" ; exit 1; fi
+if [ ! -f "$test" ] ; then echo "usage: $0 [-k] <json test file> [<word to test>]" ; exit 1; fi
 dir=`dirname "$0"`"/.."
 dir=`readlink -f "$dir"`
 lang_re=$(ls "$dir/db/" | grep '^..\.tre' | cut -c1,2 | tr '\n' '|' | sed 's/|$//')
-[ -n "$word" ] || word=`basename "$test" .json | sed -r 's/^('"${lang_re}"')\-//' | sed 's/[0-9]*$//' | sed 's/\-.*$//'`
+[ -n "$word" ] || word=$(basename "$test" .json | sed -r 's/^('"${lang_re}"')\-//' | sed 's/[0-9]*$//' | sed 's/\-.*$//')
 
 echo "Target word: $word"
 
 tre_dir=`mktemp -d /tmp/tre.XXXXXX`
 tre="${tre_dir}/dummy.tre"
 echo "$word" | $dir/tools/loadkb.py "$tre"
-cp -f $(find "$dir/db/" -name "key_shift*.ks") "$tre_dir/"
+[ -z "$no_key_shift" ] && cp -f $(find "$dir/db/" -name "key_shift*.ks") "$tre_dir/"
 
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$dir/curve/build"
 
 $dir/cli/build/cli ${CLI_OPTS} -d "$tre" "$test" 2>&1 | grep -vi '^Result:'
 
-rm -f "$tre_dir/*"
+rm -f "$tre_dir/"*
 rmdir "$tre_dir"
