0f511a0c624b7920d0a86b94dd490399eac0a892
Eric Berenguier <eric.berenguier@free.fr>
2016-08-14 08:22:17 +0200

    Build script use the right qmake version

--- build_all.sh
+++ build_all.sh
@@ -1,7 +1,11 @@
-#! /bin/sh -xe
+#! /bin/bash -xe
 
-cd `dirname $0`
-make -j9
+cd $(dirname $0)
+if [ ! -f "Makefile" ] || [ "Makefile" -ot "okboard.pro" ] ; then
+    qtchooser --run-tool=qmake -qt=5
+fi
+CPU=$(getconf _NPROCESSORS_ONLN)
+make -j"$CPU"
 cd ngrams
 python3 setup-fslm.py build
 python3 setup-cdb.py build
