0e1ea8ba05ba4d6153dc1b020fe577079ecd145e
Eric Berenguier <eric.berenguier@free.fr>
2016-12-04 09:51:19 +0100

    Better n-gram trimming (work in progress)

--- tools/clusterize.py
+++ tools/clusterize.py
@@ -12,18 +12,20 @@ import sys, os
 
 import getopt
 
-opts, args =  getopt.getopt(sys.argv[1:], 'c:w:l:')
+opts, args =  getopt.getopt(sys.argv[1:], 'c:w:l:r:')
 
 if len(args) < 1:
     print("Usage: ", os.path.basename(__file__), " [<options>] <cluster file>")
     print(" -c <count> : max cluster n-grams")
     print(" -w <count> : max word n-grams")
     print(" -l <len>   : max cluster name length (relates to binary tree depth")
+    print(" -r <file>  : output report file")
     print(" CSV data is read from stdin, clusterized data sent to stdout")
     exit(255)
 
 clusterfile = args[0]
 
+rpt_file = None
 max_word = max_cluster = max_depth = None
 for o, a in opts:
     if o == "-w":
@@ -32,6 +34,8 @@ for o, a in opts:
         max_cluster = int(a)
     elif o == "-l":
         max_depth = int(a)
+    elif o == "-r":
+        rpt_file = a
     else:
         print("Bad option: %s" % o)
         exit(1)
@@ -56,6 +60,41 @@ word2cluster['#TOTAL'] = '#CTOTAL'
 
 grams = dict()
 
+stats = dict()
+
+def update_stats(ngram, cluster, count, sign):
+    global grams
+
+    if ngram.endswith("TOTAL"): return
+    if ngram.startswith("#NA;#START;") or not ngram.startswith("#NA;"):
+        key = "3"
+    elif ngram.startswith("#NA;") and not ngram.startswith("#NA;#NA;"):
+        key = "2"
+    else: return
+
+    key += "." + ("cluster" if cluster else "word")
+    key += "." + ("total" if sign > 0 else "removed")
+
+    for id in [ "count", "coverage" ]:
+        if id == "count" and grams[ngram][0] > count: continue  # already counted
+        k = key + "." + id
+        if k not in stats: stats[k] = 0
+        value = count if id == "coverage" else 1
+        stats[k] += value
+
+def rpt_stats():
+    out = ""
+    for size in [ 2, 3 ]:
+        for type in [ "word", "cluster" ]:
+            for metric in [ "count", "coverage" ]:
+                total = stats["%d.%s.total.%s" % (size, type, metric)]
+                removed = stats.get("%d.%s.removed.%s" % (size, type, metric), 0)
+                out += ("N=%d type=%s metric=%s %d/%d %.2f%%\n" %
+                        (size, type, metric, total - removed, total, 100. * (total - removed) / total))
+    sys.stderr.write(out)
+    if rpt_file:
+        with open(rpt_file, "w") as f: f.write(out)
+
 def add_gram(l, count, cluster = False):
     global grams
 
@@ -64,6 +103,8 @@ def add_gram(l, count, cluster = False):
         grams[key] = [ count, cluster ]
     else:
         grams[key][0] += count
+
+    update_stats(key, cluster, count, 1)
 
 def truncate():
     global grams, max_cluster, max_word
@@ -78,6 +119,7 @@ def truncate():
             if len(gramlist) > max:
                 for key in gramlist[max:]:
                     if key.startswith("#NA;#NA;"): continue
+                    update_stats(key, cluster_flag, grams[key][0], -1)
                     del grams[key]
 
         if max: gramlist = gramlist[:max]
@@ -98,8 +140,12 @@ for line in sys.stdin.readlines():
     add_gram([w1, w2, w3], count)
     add_gram([w1, w2, '#TOTAL'], count)
 
-    c1, c2, c3 = word2cluster.get(w1, None), word2cluster.get(w2, None), word2cluster.get(w3, None)
-    if c1 is not None and c2 is not None and c3 is not None:
+    def cluster(w):
+        if w not in word2cluster: word2cluster[w] = "#NOCLUSTER"
+        return word2cluster[w]
+
+    c1, c2, c3 = cluster(w1), cluster(w2), cluster(w3)
+    if "#NOCLUSTER" not in (c1, c2, c3):
         add_gram([c1, c2, c3], count, True)
         add_gram([c1, c2, '#CTOTAL'], count, True)  # use separate total n-grams for words and clusters
 
@@ -117,3 +163,5 @@ truncate()
 
 for key, gram in grams.items():
     print("%d;%s" % (gram[0], key))
+
+rpt_stats()
---
+++ tools/ngram_filter.py
@@ -0,0 +1,74 @@
+#! /usr/bin/python3
+
+import sys, os
+
+if len(sys.argv) < 2:
+    print("usage:", os.path.basename(__file__), "<word coverage [0-1]> <ngram coverage [0-1]>")
+    print(" input/ouput ngram files on stdin/stdout (sorted by count)")
+    exit(1)
+
+word_coverage, ngram_coverage = float(sys.argv[1]), float(sys.argv[2])
+if word_coverage <= 0 or word_coverage > 1: raise Exception("Word coverage must be between 0 and 1")
+if ngram_coverage <= 0 or ngram_coverage > 1: raise Exception("N-gram coverage must be between 0 and 1")
+
+word_sum = ngram_sum = 0
+words = dict()
+ngrams = [ ]
+
+last_count = 0
+for line in sys.stdin:
+    count, w1, w2, w3 = line.strip().split(';')
+    count = int(count)
+
+    if count < last_count: raise Exception("Input is not sorted by count")
+    last_count = count
+
+    if w1 == '#NA' and w2 == '#NA' and w3 != '#TOTAL':
+        words[w3] = count
+        word_sum += count
+
+    ngrams.append((count, ';'.join([w1, w2, w3])))
+    ngram_sum += count
+
+def say(x): sys.stderr.write(x + "\n")
+
+# remove rare words
+new_word_sum = word_sum
+words0 = dict(words)
+word_list = sorted(words.keys(), key = lambda w: words[w])
+while new_word_sum > word_sum * word_coverage:
+    word = word_list.pop(0)
+    new_word_sum -= words[word]
+    del words[word]
+
+say("Words: %d -> %d (%.2f%%), coverage=%.2f%%" %
+    (len(words0), len(words), 100.0 * len(words) / len(words0), 100.0 * new_word_sum / word_sum))
+
+# remove n-grams using removed words
+new_ngram_sum = ngram_sum
+ngram_count0 = len(ngrams)
+new_ngrams = [ ]
+for count, ngram in ngrams:
+    if [ w for w in ngram.split(';') if w not in words and not w.startswith('#') ]:
+        new_ngram_sum -= count
+    else:
+        new_ngrams.append((count, ngram))
+
+ngrams = new_ngrams
+new_ngrams = None
+
+say("N-grams: %d -> %d (%.2f%%), coverage=%.2f%%" %
+    (ngram_count0, len(ngrams), 100.0 * len(ngrams) / ngram_count0, 100.0 * new_ngram_sum / ngram_sum))
+
+# remove rare n-grams
+for i in range(0, len(ngrams)):
+    if new_ngram_sum <= ngram_sum * ngram_coverage: break
+    count, ngram = ngrams[i]
+    new_ngram_sum -= count
+ngrams = ngrams[i:]
+
+say("N-grams: %d -> %d (%.2f%%), coverage=%.2f%%" %
+    (ngram_count0, len(ngrams), 100.0 * len(ngrams) / ngram_count0, 100.0 * new_ngram_sum / ngram_sum))
+
+for count, ngram in ngrams:
+    print("%s;%s" % (count, ngram))
